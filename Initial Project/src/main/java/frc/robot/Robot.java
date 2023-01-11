dcad531051fea63a80bd58e95eb42bf703bcd4ff
Jeremy <s0107592@tukwila.wednet.edu>
2023-01-11 15:03:32 -0800

    Test

--- Initial Project/src/main/java/frc/robot/Robot.java
+++ Initial Project/src/main/java/frc/robot/Robot.java
@@ -89,7 +89,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX())
+    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX());
   }
 
   /** This function is called once when the robot is disabled. */
